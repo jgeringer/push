3bf400ac8681a93a2bc17fdb07c3fc0a7038e941
Joseph Geringer <joegeringer@gmail.com>
2018-10-20 10:47:05 -0500

    pushing build dir

---
+++ build/precache-manifest.fcb90552e61bd1f567d5d75f3eb8d632.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7a89a0248028fc9ce972",
+    "url": "/static/js/main.7a89a024.chunk.js"
+  },
+  {
+    "revision": "795951baf08bd27ed58e",
+    "url": "/static/js/1.795951ba.chunk.js"
+  },
+  {
+    "revision": "7a89a0248028fc9ce972",
+    "url": "/static/css/main.d3003e81.chunk.css"
+  },
+  {
+    "revision": "68c1851d2bd5dfa035e5faced63955c1",
+    "url": "/index.html"
+  }
+];
